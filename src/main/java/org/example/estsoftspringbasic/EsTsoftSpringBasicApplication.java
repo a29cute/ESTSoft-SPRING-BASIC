60e54d1fefc8760784526b0f67f9d9877d9e523a
DOHA <allsun0829@gmail.com>
2024-10-04 10:31:21 +0900

    first commit

---
+++ src/main/java/org/example/estsoftspringbasic/EsTsoftSpringBasicApplication.java
@@ -0,0 +1,13 @@
+package org.example.estsoftspringbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsTsoftSpringBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsTsoftSpringBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/estsoftspringbasic/EsTsoftSpringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.estsoftspringbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsTsoftSpringBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
